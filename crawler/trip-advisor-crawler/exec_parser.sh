27d0bfcae98718edaf6ca05f14ee5a7eda854fb3
Thiago Nóbrega <thiagonobrega@uepb.edu.br>
2017-07-12 11:09:17 -0300

    correçao no nome do arquivo gerado

--- crawler/trip-advisor-crawler/exec_parser.sh
+++ crawler/trip-advisor-crawler/exec_parser.sh
@@ -7,13 +7,13 @@ process=$2
 for f in $input/*; do
     if [[ -d $f ]]; then
 	
-	out=`echo $f | awk -F '/' '{print $2}'`
+	#out=`echo $f | awk -F '/' '{print $2}'`
+	out=`echo $f | awk -F '/' '{print $NF}'`
 
 	outfile="$out.csv"
 	indir="$f/"
-#	echo $indir
-#	echo $outfile 
-	echo "python3 trip-advisor-parser2.py -d $indir -o $outfile -p $process"
+	#echo "python3 trip-advisor-parser2.py -d $indir -o $outfile -p $process"
+	python3 trip-advisor-parser2.py -d $indir -o $outfile -p $process
     fi
 done
 
